1468b7b0f30f9fc96203ba3e2cc81cfe73d24155
Thairone Loureiro <thairone.loureiro@gmail.com>
2021-06-16 20:34:33 -0300

    Update build.sh

--- sonarcloud/build.sh
+++ sonarcloud/build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo "Instalação do arduino-cli"
-wget http://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz
-tar xf arduino-cli_latest_Linux_64bit.tar.gz
-mkdir -p $HOME/bin
-mv arduino-cli $HOME/bin/arduino-cli
-export PATH=$PATH:$HOME/bin
-arduino-cli config add board_manager.additional-urls https://downloads.arduino.cc/packages/package_staging_index.json
-arduino-cli config set library.enable_unsafe_install true
-arduino-cli config dump
-arduino-cli core update-index
-arduino-cli core install arduino:megaavr
-arduino-cli lib install Servo
-arduino-cli lib install --zip-path HMC5883L.zip
-arduino-cli lib install --zip-path NewPing.zip
+echo "build.sh"
+#wget http://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz
+#tar xf arduino-cli_latest_Linux_64bit.tar.gz
+#mkdir -p $HOME/bin
+#mv arduino-cli $HOME/bin/arduino-cli
+#export PATH=$PATH:$HOME/bin
+#arduino-cli config add board_manager.additional-urls https://downloads.arduino.cc/packages/package_staging_index.json
+#arduino-cli config set library.enable_unsafe_install true
+#arduino-cli config dump
+#arduino-cli core update-index
+#arduino-cli core install arduino:megaavr
+#arduino-cli lib install Servo
+#arduino-cli lib install --zip-path HMC5883L.zip
+#arduino-cli lib install --zip-path NewPing.zip
 #mkdir -p $HOME/Arduino/libraries
 #ln -s $PWD $HOME/Arduino/libraries/.
-arduino-cli compile -b arduino:avr:mega --build-path $PWD/build sniffer_cube/sniffer_cube
+#arduino-cli compile -b arduino:avr:mega --build-path $PWD/build sniffer_cube/sniffer_cube
 #arduino-cli compile --build-path $PWD/build --fqbn arduino:avr:mega --verbose sniffer_cube/sniffer_cube -e src/software/firmware/output/sniffer_cube
 echo "#########################"
 echo "ls $PWD/build"
