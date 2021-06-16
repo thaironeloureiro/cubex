a1cfe529625b685c62d09cc22986b291a55099f8
Thairone Loureiro <thairone.loureiro@gmail.com>
2021-06-15 23:13:00 -0300

    Update build.sh

--- sonarcloud/build.sh
+++ sonarcloud/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-arduino-cli compile --build-path $PWD/build --fqbn arduino:avr:mega --verbose sniffer_cube/sniffer_cube -e src/software/firmware/output/sniffer_cube
+arduino-cli compile -b arduino:avr:mega --build-path $PWD/build sniffer_cube/sniffer_cube
+#arduino-cli compile --build-path $PWD/build --fqbn arduino:avr:mega --verbose sniffer_cube/sniffer_cube -e src/software/firmware/output/sniffer_cube
