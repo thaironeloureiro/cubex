dd512c7192c4e2e29d9cead9f5099b66aa9aa192
Thairone Loureiro <thairone.loureiro@gmail.com>
2021-06-16 13:28:30 -0300

    Update build.sh

--- sonarcloud/build.sh
+++ sonarcloud/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-wget http://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz
-tar xf arduino-cli_latest_Linux_64bit.tar.gz
-mkdir -p $HOME/bin
-mv arduino-cli $HOME/bin/arduino-cli
-export PATH=$PATH:$HOME/bin
-arduino-cli core update-index
-arduino-cli config init --additional-urls https://downloads.arduino.cc/packages/package_staging_index.json
-arduino-cli config set library.enable_unsafe_install true
-arduino-cli config dump
-arduino-cli core update-index
-arduino-cli core install arduino:megaavr
-arduino-cli lib install Servo
+#wget http://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz
+#tar xf arduino-cli_latest_Linux_64bit.tar.gz
+#mkdir -p $HOME/bin
+#mv arduino-cli $HOME/bin/arduino-cli
+#export PATH=$PATH:$HOME/bin
+#arduino-cli core update-index
+#arduino-cli config init --additional-urls https://downloads.arduino.cc/packages/package_staging_index.json
+
+#arduino-cli config set library.enable_unsafe_install true
+#arduino-cli config dump
+#arduino-cli core update-index
+#arduino-cli core install arduino:megaavr
+#arduino-cli lib install Servo
 pwd
 ls
 arduino-cli lib install --zip-path HMC5883L.zip
