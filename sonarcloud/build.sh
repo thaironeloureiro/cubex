43db8e68bcff80a6fdc146930b0a48dc1ed6447b
Thairone Loureiro <thairone.loureiro@gmail.com>
2021-06-16 13:39:02 -0300

    Update build.sh

--- sonarcloud/build.sh
+++ sonarcloud/build.sh
@@ -4,20 +4,18 @@
 #mkdir -p $HOME/bin
 #mv arduino-cli $HOME/bin/arduino-cli
 #export PATH=$PATH:$HOME/bin
-#arduino-cli core update-index
-#arduino-cli config init --additional-urls https://downloads.arduino.cc/packages/package_staging_index.json
 
-#arduino-cli config set library.enable_unsafe_install true
-#arduino-cli config dump
-#arduino-cli core update-index
-#arduino-cli core install arduino:megaavr
-#arduino-cli lib install Servo
-pwd
-ls
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+arduino-cli config init --additional-urls https://downloads.arduino.cc/packages/package_staging_index.json
+arduino-cli config set library.enable_unsafe_install true
+arduino-cli config dump
+arduino-cli core update-index
+arduino-cli core install arduino:megaavr
+arduino-cli lib install Servo
 arduino-cli lib install --zip-path HMC5883L.zip
 arduino-cli lib install --zip-path NewPing.zip
-mkdir -p $HOME/Arduino/libraries
-ln -s $PWD $HOME/Arduino/libraries/.
+#mkdir -p $HOME/Arduino/libraries
+#ln -s $PWD $HOME/Arduino/libraries/.
 arduino-cli compile -b arduino:avr:mega --build-path $PWD/build sniffer_cube/sniffer_cube
 #arduino-cli compile --build-path $PWD/build --fqbn arduino:avr:mega --verbose sniffer_cube/sniffer_cube -e src/software/firmware/output/sniffer_cube
 echo "#########################"
