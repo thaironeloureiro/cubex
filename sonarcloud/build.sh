9f9799132088156483089ddf136ab5a6326cf4b1
Thairone Loureiro <thairone.loureiro@gmail.com>
2021-06-16 20:52:55 -0300

    Update build.sh

--- sonarcloud/build.sh
+++ sonarcloud/build.sh
@@ -28,13 +28,14 @@ echo "----"
 #cat $PWD/build/compile_commands.json
 echo "----"
 echo "ls sonarcloud/bo"
-#ls sonarcloud/bo
+ls -l sonarcloud/bo
 #cat sonarcloud/bo/build-wrapper-dump.json
 #echo "--------"
 #cat sonarcloud/bo/build-wrapper.log
-echo "--------"
+echo "-------- usando convert-compile-commands.py ------"
 python convert-compile-commands.py $PWD/build/compile_commands.json
 ls cfamily-compilation-database
+echo "--------------------------------------------------"
 #cat cfamily-compilation-database/build-wrapper-dump.json
 #echo "ls $PWD/cfamily-compilation-database"
 #cat /home/runner/work/cubex/cubex/cfamily-compilation-database/build-wrapper-dump.json
