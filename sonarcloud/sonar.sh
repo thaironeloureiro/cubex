d698e5c0ec458418d671caf4940ab21a0e557051
Thairone Simões Loureiro <thairone.loureiro@gmail.com>
2021-06-17 09:55:51 -0300

    sonar-scanner - erro na versão do Java

--- sonarcloud/sonar.sh
+++ sonarcloud/sonar.sh
@@ -2,7 +2,7 @@
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo ">>>>>> unzip sonar >>>>>>>"
 unzip -o sonar-scanner-cli-3.3.0.1492-linux.zip
-echo "--- export PATH=$PATH:sonar-scanner-cli-3.3.0.1492-linux/bin ---"
-export PATH=$PATH:sonar-scanner-cli-3.3.0.1492-linux/bin
+echo "--- export PATH=$PATH:sonar-scanner-3.3.0.1492-linux/bin ---"
+export PATH=$PATH:sonar-scanner-3.3.0.1492-linux/bin
 echo "------> sonar-scanner ..."
 sonar-scanner -Dproject.settings=sonar-project.properties -X
