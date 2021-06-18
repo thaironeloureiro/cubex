73c35179e6a011806bbe35ca69807590be5ce620
Thairone Loureiro <thairone.loureiro@gmail.com>
2021-06-18 11:13:54 -0300

    Update build.sh

--- sonarcloud/build.sh
+++ sonarcloud/build.sh
@@ -44,6 +44,8 @@ ls -l cfamily-compilation-database
 rm sonarcloud/bo/build-wrapper-dump.json
 cp cfamily-compilation-database/build-wrapper-dump.json sonarcloud/bo/build-wrapper-dump.json
 echo "--------------------------------------------------"
+echo "du $HOME"
+du $HOME
 #cat cfamily-compilation-database/build-wrapper-dump.json
 #echo "ls $PWD/cfamily-compilation-database"
 #echo "#########################"
