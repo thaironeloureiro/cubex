e1154689d5e0fb2396fa5fb845bd06ca843a31e0
Thairone Loureiro <thairone.loureiro@gmail.com>
2021-06-19 10:33:04 -0300

    Update build.sh

--- sonarcloud/build.sh
+++ sonarcloud/build.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 #echo ">>>>>>>>>>>>>>>>>>>>>>>>>>"
-#ls $HOME/bin/arduino-cli
-#ls -l $HOME/bin
-#echo "build.sh"
-#wget http://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz
-#tar xf arduino-cli_latest_Linux_64bit.tar.gz
-#mkdir -p $HOME/bin
-#mv arduino-cli $HOME/bin/arduino-cli
 export PATH=$PATH:$HOME/bin
-#arduino-cli config add board_manager.additional-urls https://downloads.arduino.cc/packages/package_staging_index.json
-#arduino-cli config set library.enable_unsafe_install true
-#arduino-cli config dump
-#arduino-cli core update-index
-#arduino-cli core install arduino:megaavr
-#arduino-cli lib install Servo
-#arduino-cli lib install --zip-path HMC5883L.zip
-#arduino-cli lib install --zip-path NewPing.zip
-#mkdir -p $HOME/Arduino/libraries
-#ln -s $PWD $HOME/Arduino/libraries/.
-
 #arduino-cli compile -b arduino:avr:mega --build-path $PWD/build sniffer_cube/sniffer_cube
-arduino-cli cache clean
+#linha interessante para editar a versão do 
+#sed -Ei 's/#define HARDWARE_VERSION [0-9]+/#define HARDWARE_VERSION 2/' src/software/firmware/includes/config.h
+#sed -Ei 's/#define MODE .+/#define MODE MODE_PROD/' src/software/firmware/includes/config.h
 arduino-cli compile --build-path $PWD/build -b arduino:avr:mega --verbose sniffer_cube --output-dir $PWD/output --clean
 echo "######################### ls"
 echo "ls -l $PWD/build"
