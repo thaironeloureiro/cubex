d347550667e0ebfbec1841d8f2d146b8452137ff
Thairone Loureiro <thairone.loureiro@gmail.com>
2021-06-19 16:59:57 -0300

    Update sonar.sh

--- sonarcloud/sonar.sh
+++ sonarcloud/sonar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo ">>>>>> unzip sonar >>>>>>>>"
-unzip -o sonar-scanner-cli-4.6.2.2472-linux.zip
-echo "--- export PATH=$PATH:sonar-scanner-4.6.2.2472-linux/bin ---"
-export PATH=$PATH:sonar-scanner-4.6.2.2472-linux/bin
+unzip -o sonar-scanner-cli-4.4.0.2170-linux.zip
+echo "--- export PATH=$PATH:sonar-scanner-4.4.0.2170-linux/bin ---"
+export PATH=$PATH:sonar-scanner-4.4.0.2170-linux/bin
 echo "------> sonar-scanner ..."
 #sonar-scanner -Dproject.settings=sonar-project.properties -Dsonar.sonar.cfamily.compile-commands=build/compile_commands.json -Dsonar.verbose=true -X
 sonar-scanner -X -Dproject.settings=sonar-project.properties
