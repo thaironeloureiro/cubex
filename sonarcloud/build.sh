ca94b20228c2a115f102402ea4de0d71dab0f2ab
Thairone Loureiro <thairone.loureiro@gmail.com>
2021-06-19 17:02:59 -0300

    Update build.sh

--- sonarcloud/build.sh
+++ sonarcloud/build.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 #echo ">>>>>>>>>>>>>>>>>>>>>>>>>>"
-export PATH=$PATH:$HOME/bin
+#export PATH=$PATH:$HOME/bin
 #arduino-cli compile -b arduino:avr:mega --build-path $PWD/build sniffer_cube/sniffer_cube
 #linha interessante para editar a versão do 
 #sed -Ei 's/#define HARDWARE_VERSION [0-9]+/#define HARDWARE_VERSION 2/' src/software/firmware/includes/config.h
 #sed -Ei 's/#define MODE .+/#define MODE MODE_PROD/' src/software/firmware/includes/config.h
-arduino-cli compile --build-path $PWD/build -b arduino:avr:mega --verbose sniffer_cube --output-dir $PWD/output --clean
-echo "######################### ls"
-echo "ls -l $PWD/build"
-ls -l $PWD/build
-echo "ls -l $PWD/ouput"
-ls -l $PWD/output
-echo "---- cat $PWD/build/compile_commands.json "
-cat $PWD/build/compile_commands.json
+
+arduino-cli compile --build-path $PWD/build -b arduino:avr:mega --verbose sniffer_cube  --clean
+
+#echo "######################### ls"
+#echo "ls -l $PWD/build"
+#ls -l $PWD/build
+#echo "ls -l $PWD/ouput"
+#ls -l $PWD/output
+#echo "---- cat $PWD/build/compile_commands.json "
+#cat $PWD/build/compile_commands.json
+
 #echo "----"
 #echo "ls sonarcloud/bo"
 #ls -l sonarcloud/bo
