74b3d107f9ba207ef6ac3b2bd4e6c825eeb7e74f
Thairone Loureiro <thairone.loureiro@gmail.com>
2021-06-19 18:10:04 -0300

    Update sonar.sh

--- sonarcloud/sonar.sh
+++ sonarcloud/sonar.sh
@@ -6,4 +6,4 @@ echo "--- export PATH=$PATH:sonar-scanner-4.4.0.2170-linux/bin ---"
 export PATH=$PATH:sonar-scanner-4.4.0.2170-linux/bin
 echo "------> sonar-scanner ..."
 #sonar-scanner -Dproject.settings=sonar-project.properties -Dsonar.sonar.cfamily.compile-commands=build/compile_commands.json -Dsonar.verbose=true -X
-sonar-scanner   -Dsonar.organization=thaironeloureiro   -Dsonar.projectKey=thaironeloureiro_cubex   -Dsonar.sources=sniffer_cube   -Dsonar.cfamily.build-wrapper-output=build  -Dsonar.host.url=https://sonarcloud.io
+sonar-scanner   -Dsonar.organization=thaironeloureiro   -Dsonar.projectKey=thaironeloureiro_cubex   -Dsonar.sources=.   -Dsonar.cfamily.build-wrapper-output=build  -Dsonar.host.url=https://sonarcloud.io
